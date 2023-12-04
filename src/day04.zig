dff5b37bb44421f6f1551583fbab7ad05cf27be3
Shahruk Hossain <shahruk10@gmail.com>
2023-12-05 05:49:00 +0600

    refactor: optimize day04

--- src/day04.zig
+++ src/day04.zig
@@ -12,7 +12,10 @@ const data = @embedFile("data/day04.txt");
 
 const ParseError = error{
     LineTooShort,
+    CardNumberExceedsMax,
 };
+
+const max_card_number = 256;
 
 const PointsCounter = struct {
     data: []const u8,
@@ -20,41 +23,20 @@ const PointsCounter = struct {
     points: u32 = 0,
     total_cards_won: u32 = 0,
 
-    want_numbers_buffer: List(u16) = undefined,
-    got_numbers_buffer: List(u16) = undefined,
-    card_num_matches: Map(u16, u16) = undefined,
-    card_num_copies: Map(u16, u32) = undefined,
-    max_card_number: u16 = 0,
+    card_num_copies: [max_card_number + 1]u32 = undefined,
 
     pub fn run(self: *PointsCounter) !void {
         var lines = tokenizeAny(u8, self.data, "\n");
 
-        self.want_numbers_buffer = List(u16).init(gpa);
-        self.got_numbers_buffer = List(u16).init(gpa);
-        self.card_num_matches = Map(u16, u16).init(gpa);
-        self.card_num_copies = Map(u16, u32).init(gpa);
-
-        const max_numbers = 100;
-        const max_cards = 500;
-
-        try self.want_numbers_buffer.ensureTotalCapacity(max_numbers);
-        try self.got_numbers_buffer.ensureTotalCapacity(max_numbers);
-        try self.card_num_matches.ensureTotalCapacity(max_cards);
-        try self.card_num_copies.ensureTotalCapacity(max_cards);
-
-        defer self.want_numbers_buffer.deinit();
-        defer self.got_numbers_buffer.deinit();
-        defer self.card_num_matches.deinit();
-        defer self.card_num_copies.deinit();
-
         self.points = 0;
         self.total_cards_won = 0;
+        self.card_num_copies = [_]u32{0} ** (max_card_number + 1); // Card numbers start from 1.
 
         while (lines.next()) |line| {
             try self.parseLine(line);
         }
 
-        try self.tallyCards();
+        self.tallyCards();
     }
 
     pub fn parseLine(self: *PointsCounter, line: []const u8) !void {
@@ -85,8 +67,8 @@ const PointsCounter = struct {
         const card_number_str = trim(u8, line[4..a.?], " ");
         const card_number = try std.fmt.parseUnsigned(u16, card_number_str, 10);
 
-        if (card_number > self.max_card_number) {
-            self.max_card_number = card_number;
+        if (card_number > max_card_number) {
+            return ParseError.CardNumberExceedsMax;
         }
 
         const want_numbers_str = line[a.? + 2 .. b.? - 1];
@@ -96,88 +78,51 @@ const PointsCounter = struct {
     }
 
     pub fn update(self: *PointsCounter, card_number: u16, want_numbers_str: []const u8, got_numbers_str: []const u8) !void {
-        try self.parseNumbersFromStr(want_numbers_str, &self.want_numbers_buffer);
-        try self.parseNumbersFromStr(got_numbers_str, &self.got_numbers_buffer);
+        self.addCopyOfCard(card_number);
+
+        var want_numbers = tokenizeSeq(u8, want_numbers_str, " ");
+        var got_numbers = tokenizeSeq(u8, got_numbers_str, " ");
 
         var num_matches: u16 = 0;
 
-        for (self.want_numbers_buffer.items) |want_num| {
-            for (self.got_numbers_buffer.items) |got_num| {
-                if (want_num == got_num) {
+        while (got_numbers.next()) |got_num| {
+            want_numbers.reset();
+
+            while (want_numbers.next()) |want_num| {
+                if (std.mem.eql(u8, want_num, got_num)) {
                     num_matches += 1;
-                    break;
                 }
             }
         }
 
-        try self.card_num_matches.put(card_number, num_matches);
+        if (num_matches == 0) {
+            return;
+        }
 
-        if (num_matches > 0) {
-            self.points += std.math.pow(u32, 2, @as(u32, @intCast(num_matches)) - 1);
+        // Update points sum for given number of matches.
+        self.points += std.math.pow(u32, 2, @as(u32, @intCast(num_matches)) - 1);
+
+        // For each copy of card # i, we add a copy of the card we won off of card # i.
+        const num_copies = self.card_num_copies[card_number];
+
+        for (0..num_copies) |_| {
+            var j = card_number + 1;
+            const b = j + num_matches;
+
+            while (j < b) : (j += 1) {
+                self.addCopyOfCard(j);
+            }
         }
     }
 
-    pub fn tallyCards(self: *PointsCounter) !void {
-        for (0..self.max_card_number + 1) |card| {
-            const i = @as(u16, @intCast(card));
-            const num_matches = self.card_num_matches.get(i);
-
-            // Card number is not valid.
-            if (num_matches == null) {
-                continue;
-            }
-
-            try self.addCopyOfCard(i);
-
-            // If we didn't win any cards off of card # i, we move on to the next card.
-            if (num_matches == 0) {
-                continue;
-            }
-
-            // For each copy of card # i, we add a copy of the card we won off of card # i.
-            for (0..self.card_num_copies.get(i).?) |_| {
-                var j = i + 1;
-                const b = j + num_matches.?;
-
-                while (j < b) : (j += 1) {
-                    try self.addCopyOfCard(j);
-                }
-            }
-        }
-
-        var cards = self.card_num_copies.keyIterator();
-
-        while (cards.next()) |card| {
-            const i = card.*;
-            const num_copies = self.card_num_copies.get(i).?;
-            const num_matches = self.card_num_matches.get(i).?;
-
-            print(" - Card {d}\tmatching_numbers={d}\tcopies={d}\n", .{ i, num_matches, num_copies });
-
+    pub fn tallyCards(self: *PointsCounter) void {
+        for (self.card_num_copies) |num_copies| {
             self.total_cards_won += num_copies;
         }
     }
 
-    pub fn addCopyOfCard(self: *PointsCounter, card_num: u16) !void {
-        const num_copies = self.card_num_copies.get(card_num) orelse 0;
-        try self.card_num_copies.put(card_num, num_copies + 1);
-    }
-
-    pub fn parseNumbersFromStr(self: *PointsCounter, str: []const u8, number_list: *List(u16)) !void {
-        _ = self;
-
-        number_list.clearRetainingCapacity();
-
-        var numbers = splitSeq(u8, str, " ");
-
-        while (numbers.next()) |num_str| {
-            const num_str_trimmed = trim(u8, num_str, " ");
-            if (num_str_trimmed.len == 0) {
-                continue;
-            }
-
-            try number_list.append(try std.fmt.parseUnsigned(u16, num_str_trimmed, 10));
-        }
+    pub fn addCopyOfCard(self: *PointsCounter, card_num: u16) void {
+        self.card_num_copies[card_num] += 1;
     }
 };
 
